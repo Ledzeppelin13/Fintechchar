bfac788a211c4ada6462e81e7c728105b4c798d5
Char Siriwanya <char4152@gmail.com>
2021-01-09 09:01:47 +0700

    First commit

---
+++ contracts/Adoption.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: <SPDX-License>
+pragma solidity ^0.7.4;
+
+contract Adoption {
+    address[16] public adopters;
+
+    function adopt(uint kidId) public returns (uint) {
+        require(kidId >= 0 && kidId <=15);
+        adopters[kidId] = msg.sender;
+        return kidId;
+    }
+
+    function getAdopters() public view returns (address[16] memory) {
+        return adopters;
+    }
+}
